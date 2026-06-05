6ef00bcbbe69bdef81c2e986cba271926cb0404f
Esther Waikwa <waikwaesther0@gmail.com>
2026-06-05 12:15:45 +0300

    Update API config for deployment

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,11 +1,5 @@
-const BASE_URL = import.meta.env.VITE_API_URL;
-
 export const getWeather = async (city) => {
-  const res = await fetch(`${BASE_URL}/weather?city=${city}`);
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch weather");
-  }
-
+  const res = await fetch(`http://localhost:5000/weather?city=${city}`);
+  if (!res.ok) throw new Error("Failed to fetch weather");
   return res.json();
 };
